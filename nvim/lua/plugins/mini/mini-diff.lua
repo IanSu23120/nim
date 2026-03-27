d45e24a8f7ad0ea3ed231c918c442035144a084d
iansu <4B1L0901@stust.edu.tw>
2026-03-27 15:00:04 +0800

    add mini-dff

---
+++ nvim/lua/plugins/mini/mini-diff.lua
@@ -0,0 +1,12 @@
+return {
+	"mini.diff",
+	event = "DeferredUIEnter",
+	after = function()
+		require("mini.diff").setup({
+			view = {
+				style = "sign",
+				signs = { add = "+", change = "@", delete = "-" },
+			},
+		})
+	end,
+}
